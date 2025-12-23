823f54d82c32105490ae47a759f3c81cec154608
enzomaass-cmyk <enzo_maass@estudante.sesisenai.org.br>
2025-12-23 13:36:36 -0300

    exercicio 1 feito

---
+++ Exercicio1.java
@@ -0,0 +1,11 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+        //TODO: Chame o método saudar
+        saudar();
+    }
+
+    //TODO: Crie o método saudar
+    public static void saudar() {
+        System.out.println("Olá, mundo!");
+    }
+}
