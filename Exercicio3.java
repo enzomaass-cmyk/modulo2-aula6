6f293dbb26f669d0c5cb21f20319a7a4a5bf39b5
enzomaass-cmyk <enzo_maass@estudante.sesisenai.org.br>
2025-12-23 13:39:35 -0300

    exercicio 3 feito

---
+++ Exercicio3.java
@@ -0,0 +1,15 @@
+public class Exercicio3 {
+    public static void main(String[] args) {
+        //TODO: Chame o método somar e exiba o resultado
+        int resultado = somar(5, 3);
+        System.out.println("Soma: " + resultado);
+
+        int resultado2 = somar(10, 20);
+        System.out.println("Soma: " + resultado2);
+    }
+
+    //TODO: Crie o método somar
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+}
