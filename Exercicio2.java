e37020eaeeb923b2499ae44160a3c399d49866c7
enzomaass-cmyk <enzo_maass@estudante.sesisenai.org.br>
2025-12-23 13:38:14 -0300

    exercicio 2 feito

---
+++ Exercicio2.java
@@ -0,0 +1,13 @@
+public class Exercicio2 {
+    public static void main(String[] args) {
+        //TODO: Chame o método saudar com um nome
+        saudar("João");
+        saudar("Maria");
+        saudar("Pedro");
+    }
+
+    //TODO: Crie o método saudar com parâmetro
+    public static void saudar(String nome) {
+        System.out.println("Olá, " + nome);
+    }
+}
