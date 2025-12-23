0e6086f3b6a79d1b1333e62005938cc5371955ae
enzomaass-cmyk <enzo_maass@estudante.sesisenai.org.br>
2025-12-23 13:40:59 -0300

    exercicio4 feito

---
+++ Exercicio4.java
@@ -0,0 +1,17 @@
+public class Exercicio4 {
+    public static void main(String[] args) {
+        //TODO: Chame o método somarTres
+        int resultado = somarTres(5, 3, 2);
+        System.out.println("Soma: " + resultado);
+
+        int resultado2 = somarTres(10, 20, 30);
+        System.out.println("Soma: " + resultado2);
+    }
+
+    //TODO: Crie o método somarTres
+    public static int somarTres(int a, int b, int c) {
+        return a + b + c;
+    }
+} 
+    
+
